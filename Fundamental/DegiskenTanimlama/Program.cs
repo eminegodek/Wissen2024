40234fb2d6cbe955973da46ba21a5d4b2d2adec7
eminegodek <eminegodekk@gmail.com>
2024-08-07 21:08:30 +0300

    console

---
+++ Fundamental/DegiskenTanimlama/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DegiskenTanimlama
+{
+    internal class Program
+    {
+        static void Main(string[] args) //bu bir metod
+        {
+            #region Degisken Yazim Tavsiyeleri
+            // pascal case => WriteLine
+            // camel case => writeLine
+            // snake case => write.line
+            // kebap case => write-line
+            #endregion
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ben Emine!");
+        }
+    }
+}
